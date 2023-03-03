8e5bf821c6ecdbe29d843359a2e24414bc49bc09
Elton <eltoncavele8@gmail.com>
2023-03-03 02:37:24 -0800

    fix:create Dbset

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -13,5 +13,18 @@ public class DataContext: IdentityDbContext
         
     }
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Favorite> Favorites { get; set; }
+    public DbSet<Loan> Loans { get; set; }
+    public DbSet<Permission> Permissions { get; set; }
+    public DbSet<Purchase> Purchases { get; set; }
+    public DbSet<Request> Requests { get; set; }
+    public DbSet<RequestBook> RequestBooks { get; set; }
+    public DbSet<Reserve> Reserves { get; set; }
+    public DbSet<RolePermission> RolePermissions { get; set; }
+    public DbSet<Supplier> Suppliers { get; set; }
 
 }
